f2e91472b29858336c7f1a9f18e83aff40cb7001
Romain Ait Abdelmalek-Lomenech <romain.ait@centralesupelec.fr>
2024-07-04 16:31:00 +0200

    demo_qsi: fix paths

--- scripts/demo_qsi.m
+++ scripts/demo_qsi.m
@@ -25,6 +25,8 @@ funct_config = @branin_mod_config;
 
 warning 'off'
 here = fileparts(mfilename('fullpath')); %local path
+data_dir = fullfile(here, '..', 'data');
+
 prm = funct_struct(); %call to function parameters to check for error in dimension for demo
 config = funct_config();
 
@@ -32,15 +34,15 @@ if (prm.dim_x ~= 1) || (prm.dim_s ~= 1)
     error("Invalid problem dimension")
 end
 
-generate_grid(funct_struct, funct_config, ID);
-QSI_SUR(funct_struct, funct_config, ID, 'data', DEMO);
+generate_doe_init(funct_struct, funct_config, ID, data_dir);
+QSI_SUR(funct_struct, funct_config, ID, data_dir, DEMO);
 
 %Deleting saved datas
 filename = sprintf('doe_init_%s_init_%d.csv', prm.name, ID); %delete grid
-delete(fullfile(here, 'data/doe_init', filename))
+delete(fullfile(data_dir, 'doe_init', filename))
 filename = sprintf('doe_QSI_%s_%s_%d.csv', config.critName, prm.name, ID); %delete DoE
-delete(fullfile(here, 'data/results/design', filename))
+delete(fullfile(data_dir, 'results/design', filename))
 filename = sprintf('cov_QSI_%s_1_%s_%d.csv', config.critName, prm.name, ID); %delete cov model
-delete(fullfile(here, 'data/results/param', filename))
+delete(fullfile(data_dir, 'results/param', filename))
 filename = sprintf('param_QSI_%s_1_%s_%d.csv', config.critName, prm.name, ID); %delete cov param
-delete(fullfile(here, 'data/results/param', filename))
+delete(fullfile(data_dir, 'results/param', filename))
