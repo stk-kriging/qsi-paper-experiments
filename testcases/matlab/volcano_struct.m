f77a278d27aef852f0595fd9ffacc64b12c22563
RomainAit <Romain.Ait@outlook.com>
2026-06-30 21:02:45 +0200

    algorithms/stk-contrib-qsi: Update to f5d19a4

--- testcases/matlab/volcano_struct.m
+++ testcases/matlab/volcano_struct.m
@@ -15,17 +15,16 @@ prm.BOX = stk_hrect(repmat([0;1],1,prm.dim_x+prm.dim_s));
 prm.BOXx = stk_hrect(repmat([0;1],1,prm.dim_x));
 prm.BOXs = stk_hrect(repmat([0;1],1,prm.dim_s));
 
-
 prm.const = [0.015 ; inf];
 prm.alpha = 0.9;
 
-
 prm.name = "volcano";
-prm.name_sub = ["volcano"];
+prm.name_sub = "volcano";
 
+prm.list_cov = {             ...
+    'stk_expcov_aniso';      ...
+    'stk_materncov32_aniso'; ...
+    'stk_materncov52_aniso'; ...
+    'stk_gausscov_aniso'     };
 
-
-prm.list_cov = ["stk_expcov_aniso"; "stk_materncov32_aniso"; "stk_materncov52_aniso"; "stk_gausscov_aniso"];
-
-
-end
+end % function
